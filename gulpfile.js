a9c6763c00f03da77378090bc09a405b3fd81e8b
Morten Berg <mortenberg80@gmail.com>
2016-01-18 22:12:49 +0100

    Added build-steps

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 // Gulp Dependencies
 var gulp = require('gulp');
-var rename = require('rename');
+var rename = require('gulp-rename');
 
 // Build Dependencies
 var browserify = require('gulp-browserify');
@@ -28,4 +28,28 @@ gulp.task('lint-test', function() {
   return gulp.src('./test/**/*.js')
                                 .pipe(jshint())
                                 .pipe(jshint.reporter('default'));
+});
+
+gulp.task('browserify-client', ['lint-client'], function() {
+  return gulp.src('client/index.js')
+    .pipe(browserify({
+      insertGlobals: true
+    }))
+    .pipe(rename('iterate-timeline.js'))
+    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest('public/javascripts'))
+});
+
+gulp.task('browserify-test', ['lint-test'], function() {
+  return gulp.src('test/client/index.js')
+    .pipe(browserify({
+      insertGlobals: true
+    }))
+    .pipe(rename('client-test.js'))
+    .pipe(gulp.dest('build'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('client/**/*.js', ['browserify-client']);
+  gulp.watch('test/client/**/*.js', ['browserify-tets']);
 });
